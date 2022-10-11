1cdff31edcf8078c9d5a487db8bfc2e05fea7c24
U-BSD\462750 <462750@SHS-DGCVCS2.beaverton.k12.or.us>
2022-10-11 12:14:24 -0700

    second

--- list.cpp
+++ list.cpp
@@ -1,3 +1,24 @@
 /*
-testestestest
+Tej Hiremath
+-
+-
 */
+
+#include <iostream>
+using namespace std;
+
+struct Students {
+
+  char name[10];
+  int id;
+  float gpa;
+};
+
+int main() {
+  Students george;
+  cin >> george.name;
+  george.id = 1234;
+  george.gpa = 3.1;
+  cout << george.name << " ID: " << george.id << " GPA: ";
+  cout << george.gpa << endl;
+}
