c7fc57564f787a67f9af843af202a24fedda559c
U-BSD\462750 <462750@SHS-DGCVCS2.beaverton.k12.or.us>
2022-10-13 12:18:42 -0700

    how to add identifiers:

--- list.cpp
+++ list.cpp
@@ -9,16 +9,40 @@ using namespace std;
 
 struct Students {
 
-  char name[10];
+  char FirstName[10];
+  char LastName[10];
   int id;
   float gpa;
 };
 
+
+void READ () {
+  Students roll;
+
+  cout << "First name?" << endl;
+  cin >> roll.FirstName;
+  cout << "Last name?" << endl;
+  cin >> roll.LastName;
+  cout << "GPA?" << endl;
+  cin >> roll.gpa;
+  cout << "ID?" << endl;
+  cin >> roll.id;
+  cout << "------------------------------------------------------" << endl;
+  cout << roll.FirstName << " " << roll.LastName << " has been registered." << endl;
+
+  cout << "ID: " << roll.id << endl;
+  cout << "GPA: " << roll.gpa << endl;
+}
+
 int main() {
-  Students george;
-  cin >> george.name;
-  george.id = 1234;
-  george.gpa = 3.1;
-  cout << george.name << " ID: " << george.id << " GPA: ";
-  cout << george.gpa << endl;
+  //Students george;
+  //cin >> george.FirstName;
+  //cin >> george.LastName;
+  //george.id = 1234;
+  //george.gpa = 3.1;
+  //cout << george.FirstName << " " << george.LastName << " ID: " << george.id << " GPA: ";
+  //cout << george.gpa << endl;
+   
+  
+  READ();
 }
