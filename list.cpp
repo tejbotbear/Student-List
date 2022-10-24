af04be2aeb2355defe1dffac8673c3b46434b6cf
U-BSD\462750 <462750@SHS-DGCVCS2.beaverton.k12.or.us>
2022-10-24 12:18:42 -0700

    figure it out i dont know what im doing wrong

--- list.cpp
+++ list.cpp
@@ -5,39 +5,63 @@ Tej Hiremath
 */
 
 #include <iostream>
+#include <vector>
+#include <stdlib.h>
 using namespace std;
 
 struct Students {
-
   char FirstName[10];
   char LastName[10];
   int id;
   float gpa;
 };
 
-void READ (int& i, Students roll[10]) {
-  //Students roll[i];
-  i = 1;
+void ADD(vector<Students*> v);
 
+void ACTION(int& i, vector<Students*> v) {
+  char Input = ' ';
+  bool Valid = false;
+  Valid = false;
+  do {
+    cout << "Enter 'p' to print a students data, or 'a' to add data." << endl;
+    Input = ' ';
+    cin >> Input;
+    if (Input == 'p' && Input == 'a') {
+      cout << "Not a valid input" << endl;
+    } 
+    else if (Input == 'p') {
+      cout << "----------------------------------------------" << endl;
+      cout << (*s).FirstName << " " << (*s).LastName << endl;
+      cout << "GPA: " << (*s).gpa << endl;
+      cout << "ID: " << (*s).id << endl;
+      Valid = true;
+    }
+    else if (Input == 'a') {
+      ADD(v);
+      Valid = true;
+    }
+  } while (Valid == false);
+}
+
+void ADD(vector<Students*> v) {
+  //Students *s = new Students();
   cout << "First name?" << endl;
-  cin >> roll[i].FirstName;
+  cin >> (*s).FirstName;
   cout << "Last name?" << endl;
-  cin >> roll[i].LastName;
+  cin >> (*s).LastName;
   cout << "GPA?" << endl;
-  cin >> roll[i].gpa;
+  cin >> (*s).gpa;
   cout << "ID?" << endl;
-  cin >> roll[i].id;
+  cin >> (*s).id;
   cout << "------------------------------------------------------" << endl;
-  cout << roll[i].FirstName << " " << roll[i].LastName << " has been registered." << endl;
-
-  cout << "ID: " << roll[i].id << endl;
-  cout << "GPA: " << roll[i].gpa << endl;
+  cout << (*s).FirstName << " " << (*s).LastName << " has been registered." << endl;
+  cout << "ID: " << (*s).id << endl;
+  cout << "GPA: " << (*s).gpa << endl;
 }
 
 int main() {
   int count = 0;
   int& i = count;
-  Students roll[10];
-  
-  READ(i, roll);
+  vector<Students*> v;
+  ACTION(i, v);
 }
