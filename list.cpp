2f2d4b4a6853876ace555dc3fbe5b0445bf3bf36
U-BSD\462750 <462750@SHS-DGCVCS2.beaverton.k12.or.us>
2022-10-26 12:24:59 -0700

    102622

--- list.cpp
+++ list.cpp
@@ -9,42 +9,49 @@ Tej Hiremath
 #include <stdlib.h>
 using namespace std;
 
-struct Students {
+struct Student {
   char FirstName[10];
   char LastName[10];
   int id;
   float gpa;
 };
 
-void ADD(vector<Students*> v);
+void ADD(int& i,vector<Student*> &v, Student *s);
 
-void ACTION(int& i, vector<Students*> v) {
+void ACTION(int& i, vector<Student*> &v, Student *s) {
   char Input = ' ';
   bool Valid = false;
   Valid = false;
   do {
-    cout << "Enter 'p' to print a students data, or 'a' to add data." << endl;
+    cout << "Enter 'p' to print a students data, or 'a' to add data. In addition 'd' can delete a students data." << endl;
     Input = ' ';
     cin >> Input;
-    if (Input == 'p' && Input == 'a') {
+    if (Input == 'p' && Input == 'a' && Input == 'd') {
       cout << "Not a valid input" << endl;
     } 
     else if (Input == 'p') {
-      cout << "----------------------------------------------" << endl;
-      cout << (*s).FirstName << " " << (*s).LastName << endl;
-      cout << "GPA: " << (*s).gpa << endl;
-      cout << "ID: " << (*s).id << endl;
-      Valid = true;
+      for (int j = 0; j < i; j++) {	
+	Student *s = v[j];
+	cout << "----------------------------------------------" << endl;
+	cout << (*s).FirstName << " " << (*s).LastName << endl;
+	cout << "GPA: " << (*s).gpa << endl;
+	cout << "ID: " << (*s).id << endl;
+	cout << "----------------------------------------------" << endl;
+      }
+    }
+    else if (Input == 'd') {
+      
     }
     else if (Input == 'a') {
-      ADD(v);
+      i++;
+      ADD(i, v, s);
       Valid = true;
     }
   } while (Valid == false);
 }
 
-void ADD(vector<Students*> v) {
-  //Students *s = new Students();
+void ADD(int& i, vector<Student*> &v, Student *s) {
+  v.push_back(s);
   cout << "First name?" << endl;
   cin >> (*s).FirstName;
   cout << "Last name?" << endl;
@@ -53,15 +60,19 @@ void ADD(vector<Students*> v) {
   cin >> (*s).gpa;
   cout << "ID?" << endl;
   cin >> (*s).id;
-  cout << "------------------------------------------------------" << endl;
+  cout << "----------------------------------------------" << endl;
   cout << (*s).FirstName << " " << (*s).LastName << " has been registered." << endl;
   cout << "ID: " << (*s).id << endl;
   cout << "GPA: " << (*s).gpa << endl;
+  cout << "----------------------------------------------" << endl;
+  //v.push_back(s);
+  ACTION(i, v, s);
 }
 
 int main() {
   int count = 0;
   int& i = count;
-  vector<Students*> v;
-  ACTION(i, v);
+  vector<Student*> v;
+  Student *s = new Student();
+  ACTION(i, v, s);
 }
