8b97466bffd810566caf2a04c644aa1bd25e63b8
tejbotbear <89226541+tejbotbear@users.noreply.github.com>
2022-11-04 13:32:19 -0700

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -1,9 +1,8 @@
-/*
+/* 
 Tej Hiremath
--
--
+This program uses structs, pointers, and vectors to allow the user to input, read and terminate student data.
+11/4/22
 */
-
 #include <iostream>
 #include <vector>
 #include <stdlib.h>
@@ -11,97 +10,60 @@ Tej Hiremath
 using namespace std;
 
 struct Student {
-  char FirstName[10];
-  char LastName[10];
-  int id;
-  float gpa;
+    char FirstName[10];
+    char LastName[10];
+    int id;
+    float gpa;
 };
 
-void DELETE(int& i, vector<Student*> &v, Student *s);
-void ADD(int& i,vector<Student*> &v, Student *s);
-
-void ACTION(int& i, vector<Student*> &v, Student *s) {
-  char Input = ' ';
-  bool Valid = false;
-  do {
-    cout << "Enter 'p' to print a students data, or 'a' to add data. In addition 'd' can delete a students data." << endl;
-    //Input = ' ';
-    cin >> Input;
-    if (Input == 'p' && Input == 'a' && Input == 'd') {
-      cout << "Not a valid input" << endl;
-    } 
-    else if (Input == 'p') {
-      int i = v.size();
-      for (int j = 0; j < i; j++) {	
-	Student *s = v[j];
-	cout << "----------------------------------------------" << endl;
-	cout << (*s).FirstName << " " << (*s).LastName << endl;
-	cout << "GPA: " << (*s).gpa << endl;
-	cout << "ID: " << (*s).id << endl;
-	cout << "----------------------------------------------" << endl;
-      }
-    }
-    else if (Input == 'd') {
-      DELETE(i, v, s);
-    }
-    else if (Input == 'a') {
-      Student *NewStudent = new Student();
-      ADD(i, v, NewStudent);
-      Valid = true;
-    }
-  } while (Valid == false);
-}
-
-void DELETE(int& i, vector<Student*> &v, Student *s) {
-  int DeleteID;
-  char ConfirmDelete[6];
-  char ref[6] = {'D', 'E', 'L', 'E', 'T', 'E'};
-  cout << "Enter the ID of the student's data you are trying to remove" << endl;
-  cin >> DeleteID;
-  cin.ignore();
-  for (int j = 0; j < i; j++) {
-    Student *s = v[j];
-    if (DeleteID == (*s).id) {
-      cout << "Are you sure you want to remove " << (*s).FirstName << " " << (*s).LastName << " from the list? Type 'DELETE' to confirm." << endl;
-      //cin.ignore();
-      cin.getline(ConfirmDelete, 6);
-      if (ConfirmDelete == ref) {
-	cout << (*s).FirstName << " " << (*s).LastName << " has been removed from the list." << endl;
-	delete s;
-	v.erase(v.begin()+j);
-	--i;
-	ACTION(i,v,s);
-      }
-      else {
-	cout << "Going back to action menu" << endl;
-	ACTION(i,v,s);
-      }
-    }
-  }
-}
-
-void ADD(int& i, vector<Student*> &v, Student *s) {
-  cout << "First name?" << endl;
-  cin >> (*s).FirstName;
-  cout << "Last name?" << endl;
-  cin >> (*s).LastName;
-  cout << "GPA?" << endl;
-  cin >> (*s).gpa;
-  cout << "ID?" << endl;
-  cin >> (*s).id;
-  cout << "----------------------------------------------" << endl;
-  cout << (*s).FirstName << " " << (*s).LastName << " has been registered." << endl;
-  cout << "ID: " << (*s).id << endl;
-  cout << "GPA: " << (*s).gpa << endl;
-  cout << "----------------------------------------------" << endl;
-  v.push_back(s);
-  ACTION(i, v, s);
-}
+void DELETE(vector<Student*>& v, Student* s);
+void ADD(vector<Student*>& v, Student* s);
+void ACTION(vector<Student*>& v, Student* s);
 
 int main() {
-  int count = 0;
-  int& i = count;
-  vector<Student*> v;
-  Student *s = new Student();
-  ACTION(i, v, s);
+    vector<Student*> v;
+    Student* s = new Student();
+    ACTION(v, s);
+}
+
+void ACTION(vector<Student*>& v, Student* s) {
+
+    //Variable Initialization
+    char input[21] = { ' ' };
+    char quit[] = "QUIT";
+    char print[] = "PRINT";
+    char add[] = "ADD";
+    char del[] = "DELETE";
+    bool valid = false;
+
+    //Loop
+    do {
+        cout << "Enter 'ADD' to add new data, 'PRINT' to display existing data, or 'DELETE' to delete a students data" << endl;
+        cin.getline(input, 21);
+        if (!strcmp(input, add)) {
+            Student* NewStudent = new Student();
+            ADD(v, NewStudent);
+            valid = true;
+        }
+        else if (!strcmp(input, print)) {
+            int i = v.size();
+            for (int j = 0; j < i; j++) {
+                Student* s = v[j];
+                cout << "----------------------------------------------" << endl;
+                cout << (*s).FirstName << " " << (*s).LastName << endl;
+                cout << "GPA: " << (*s).gpa << endl;
+                cout << "ID: " << (*s).id << endl;
+                cout << "----------------------------------------------" << endl;
+            }
+        }
+        else if (!strcmp(input, del)) {
+            DELETE(v, s);
+        }
+        else if (!strcmp(input, quit)) {
+            cout << "Exiting Program";
+        }
+        else {
+          cout << "Not a valid input" << endl;
+        }
+    } while (strcmp(input, quit));
 }
