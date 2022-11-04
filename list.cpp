c54d2d3715a40bedf890f34bc07985f14152dc9b
tej <tejbotbear@gmail.com>
2022-11-04 18:31:38 +0100

    forbosch

--- list.cpp
+++ list.cpp
@@ -7,6 +7,7 @@ Tej Hiremath
 #include <iostream>
 #include <vector>
 #include <stdlib.h>
+#include <cstring>
 using namespace std;
 
 struct Student {
@@ -22,15 +23,15 @@ void ADD(int& i,vector<Student*> &v, Student *s);
 void ACTION(int& i, vector<Student*> &v, Student *s) {
   char Input = ' ';
   bool Valid = false;
-  Valid = false;
   do {
     cout << "Enter 'p' to print a students data, or 'a' to add data. In addition 'd' can delete a students data." << endl;
-    Input = ' ';
+    //Input = ' ';
     cin >> Input;
     if (Input == 'p' && Input == 'a' && Input == 'd') {
       cout << "Not a valid input" << endl;
     } 
     else if (Input == 'p') {
+      int i = v.size();
       for (int j = 0; j < i; j++) {	
 	Student *s = v[j];
 	cout << "----------------------------------------------" << endl;
@@ -45,31 +46,31 @@ void ACTION(int& i, vector<Student*> &v, Student *s) {
     }
     else if (Input == 'a') {
       Student *NewStudent = new Student();
-      ++i;
       ADD(i, v, NewStudent);
       Valid = true;
     }
   } while (Valid == false);
 }
 
-
 void DELETE(int& i, vector<Student*> &v, Student *s) {
-
   int DeleteID;
-  char ConfirmDelete[6];// = {' ', ' ', ' ', ' ', ' ', ' '};
+  char ConfirmDelete[6];
   char ref[6] = {'D', 'E', 'L', 'E', 'T', 'E'};
   cout << "Enter the ID of the student's data you are trying to remove" << endl;
   cin >> DeleteID;
-
+  cin.ignore();
   for (int j = 0; j < i; j++) {
     Student *s = v[j];
     if (DeleteID == (*s).id) {
       cout << "Are you sure you want to remove " << (*s).FirstName << " " << (*s).LastName << " from the list? Type 'DELETE' to confirm." << endl;
+      //cin.ignore();
       cin.getline(ConfirmDelete, 6);
       if (ConfirmDelete == ref) {
 	cout << (*s).FirstName << " " << (*s).LastName << " has been removed from the list." << endl;
 	delete s;
 	v.erase(v.begin()+j);
+	--i;
+	ACTION(i,v,s);
       }
       else {
 	cout << "Going back to action menu" << endl;
@@ -78,8 +79,6 @@ void DELETE(int& i, vector<Student*> &v, Student *s) {
     }
   }
 }
-
-
 
 void ADD(int& i, vector<Student*> &v, Student *s) {
   cout << "First name?" << endl;
